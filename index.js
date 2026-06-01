e2cee1365731886ebc2620e60e1e7fec8c24c259
Christian99909 <cristian99909@gmail.com>
2026-06-01 09:22:14 -0600

    Se configura la conexión a la base de datos y actualiza las dependencias

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import dotenv from 'dotenv'
+import conectarDB from './config/db.js'
 
 const app = express();
+dotenv.config();
+
+conectarDB();
 
 app.use('/', (req, res) => {
 

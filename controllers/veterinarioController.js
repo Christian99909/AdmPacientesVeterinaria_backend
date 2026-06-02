78c752afbcd0e98fbe1cb42a2cfb82e963b33c5c
Christian99909 <cristian99909@gmail.com>
2026-06-02 09:07:12 -0600

    Agrega controlador Veterinario y rutas para registro y mostrar perfiles

---
+++ controllers/veterinarioController.js
@@ -0,0 +1,30 @@
+import Veterinario from "../models/Veterinario.js"
+
+
+const registrar = async (req, res) => {
+    
+    //const {email, password, nombre} = req.body;
+    
+    try {
+
+        //Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+
+        res.json(veterinarioGuardado);
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
+};
+
+const perfil = (req, res) => {
+
+    res.json({msg: "Mostrando Perfil" });
+
+};
+
+
+export { registrar, perfil }

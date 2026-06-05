fb14c7705ed2816e92549cd56360771cf0afa378
Christian99909 <cristian99909@gmail.com>
2026-06-04 21:09:32 -0600

    Agrega función de confirmación de cuenta y ruta correspondiente en el controlador Veterinario

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -38,4 +38,31 @@ const perfil = (req, res) => {
 };
 
 
-export { registrar, perfil }
+const confirmar = async (req, res) => {
+
+    const {token} = req.params
+
+    const usuarioConfirmar = await Veterinario.findOne({token});
+
+    if (!usuarioConfirmar) {
+        
+        const error = new Error('Token no válido');
+
+        return res.status(400).json({msg: error.message});
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({msg: 'Confirmando cuenta...'});
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
+};
+
+
+export { registrar, perfil, confirmar }

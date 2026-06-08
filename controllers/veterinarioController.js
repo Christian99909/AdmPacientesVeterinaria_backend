2ff21855c95271118ff4cbe1662724708eb1a71c
Christian99909 <cristian99909@gmail.com>
2026-06-07 22:45:07 -0600

    Corrige la función de autenticación para que busque el usuario por email y maneje errores adecuadamente

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -64,12 +64,21 @@ const confirmar = async (req, res) => {
 
 };
 
-const autenticar = (req, res) => {
+const autenticar = async (req, res) => {
 
-    console.log(req.body);
+    const {email} = req.body;
+
+    const usuario = await Veterinario.findOne({email});
+
+ 
+    if (!usuario) {
+        
+        const error = new Error('El Usuario no existe');
+
+        return res.status(400).json({msg: error.message});
+    }
     
-    res.json({msg: 'Autenticando'})
-}
+};
 
 
 export { registrar, perfil, confirmar, autenticar }
